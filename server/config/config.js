aa31245d245721cf665339c4d63f480b4e94a73a
Luis David Chávez Grimaldo <luiscg404@gmail.com>
2020-11-05 18:51:01 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 //==========================
 
 process.env.PORT = process.env.PORT || 3000
+
+//===========================
+//Entorno
+//==========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//===========================
+//Base de datos
+//==========================
+let urlDB;
+
+//if (process.env.NODE_ENV === 'dev') {
+//   urlDB = 'mongodb://localhost:27017/cafe'
+
+//} else {
+urlDB = 'mongodb+srv://chagri98:myhckxM0X2uRLO6z@cluster0.tjgvd.mongodb.net/cafe'
+    //}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,23 @@
 require('./config/config');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
-app.post('/usuario', function(req, res) {
-    let persona = req.body;
+app.use(require('./routes/api.js'));
 
-    if (persona.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        })
-    } else {
-        res.json({ persona });
-    }
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
+mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }).then(db => console.log('conexion exitosa'))
+    .catch(err => console.log('error: ', err))
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto 3000");
