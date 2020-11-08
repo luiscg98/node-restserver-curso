4c7c71cdb2c1881fb2e7616af13cce146ac3dc5b
Luis David Chávez Grimaldo <luiscg404@gmail.com>
2020-11-07 18:14:11 -0600

    se hicieron adecuaciones a usuario.js

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -1,48 +1,54 @@
 const jwt = require('jsonwebtoken');
 
-//================================
-//Verificar Token
-//================================
 
+// =====================
+// Verificar Token
+// =====================
 let verificaToken = (req, res, next) => {
 
     let token = req.get('Authorization');
+
     jwt.verify(token, process.env.SEED, (err, decoded) => {
+
         if (err) {
             return res.status(401).json({
                 ok: false,
                 err: {
-                    message: "Token no valido"
+                    message: 'Token no válido'
                 }
-            })
+            });
         }
+
+        req.usuario = decoded.usuario;
         next();
-    })
+
+    });
+
+
 
 };
 
-//================================
-//Verificar AdminRole
-//================================
-
+// =====================
+// Verifica AdminRole
+// =====================
 let verificaAdmin_Role = (req, res, next) => {
 
     let usuario = req.usuario;
 
-    if (usuario.role == "ADMIN_ROLE") {
+    if (usuario.role === 'ADMIN_ROLE') {
         next();
     } else {
-        return res.status(401).json({
+
+        return res.json({
             ok: false,
             err: {
-                message: "El usuario no es administrador"
+                message: 'El usuario no es administrador'
             }
-        })
+        });
     }
-
-
-
 };
+
+
 
 module.exports = {
     verificaToken,
